03dffa160f83ed84250fcb4197bbd59f8d0618de
TYM-12 <thanushraj385@gmail.com>
2025-11-01 18:47:32 +0800

    corsProxy alternative

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -5,7 +5,7 @@ const CONFIG = {
     username: 'theyoungmaker', //TODO: Your Scratch username. (Required)
     limit: 5, // How many projects to display; maximum 20
     sortBy: 'date', // date | views | remixes
-    corsProxy: 'https://scratchapicors-8nbi5t6td-theyoungmaker.vercel.app/', //TODO: Enter your CORS Proxy here
+    corsProxy: 'https://api.allorigins.win/raw?url=', //TODO: Enter your CORS Proxy here
   },
   github: {
     username: 'TYM-12', // Your GitHub org/user name. (This is the only required config)
@@ -18,7 +18,7 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 0, // How many projects to display.
+        limit: 2, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
